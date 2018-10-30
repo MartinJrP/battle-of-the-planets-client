d82c8dd8bbf6efa5977eeed4f02a2d180f4ad034
Leandro <leandrofernandez97@hotmail.com>
2018-10-30 16:41:09 -0400

    Add two components and trigger which is viewed on click of button

---
+++ src/components/TheGameSession.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="the-game-session">
+        <h1>The Game Session</h1>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'TheGameSession'
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/TheHomeScreen.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="the-home-screen">
+
+        <div class="container">
+
+            <h1>Battle of the Planets</h1>
+
+            <p class="description">Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu. Natoque leo penatibus et leo magnis dis parturient montes.</p>
+
+            <button class="main-button" v-on:click="emitStartGameEvent">Start Game</button>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TheHomeScreen',
+    methods: {
+        emitStartGameEvent: function () {
+            this.$emit("start-game");
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#the-home-screen {
+
+}
+</style>
