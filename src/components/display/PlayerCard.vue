a20fb58ccfb3fe0471af45fe33929790d95b4614
Leandro <leandrofernandez97@hotmail.com>
2018-11-01 19:44:24 -0400

    Refactored connected players layout
    Exported planet images into pngs that are all 106px X 106px. Added folders "remote" and "display" to seperate components and redirected them in the AppDisplay.vue and AppRemote.vue files. Added a PlayerCard.vue component to remote folder that holds the styling and code to loop and display the players connected on the GameSession.vue page.

---
+++ src/components/display/PlayerCard.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div id="player-card" class="player-card">
+        <img
+            v-bind:src="path" 
+            alt="Planet Avatar" 
+            class="planet-avatar">
+
+        <p class="player-name"> {{ player.username }}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+name: "PlayerCard",
+props: ["player"],
+computed: {
+    baseUrl: function () {
+        return process.env.BASE_URL
+    },
+    path: function () {
+        var path = `${this.baseUrl}img/planet-${ this.player.num }.png`
+        return path
+    }
+}
+
+}
+</script>
+
+<style lang="scss">
+
+.player-card {
+    padding:0px 40px 0px 0px;
+}
+
+.player-card:last-of-type,
+.player-card:nth-of-type(6) {
+    padding:0px 0px 0px 0px;
+}
+
+.planet-avatar {
+    width:106px;
+}
+
+.player-name {
+    font-size:16px;
+    font-weight:700;
+    font-family: "Open Sans", sans-serif;
+    text-align: center;
+    margin-top:0;
+}
+
+</style>
---
+++ src/components/display/TheGameSession.vue
@@ -0,0 +1,109 @@
+<template>
+    <div id="the-game-session">
+        <div class="game-code-container">
+
+            <h1>Battle of the Planets</h1>
+
+            <h2>Game Code</h2>
+
+            <p class="game-code"> {{ code }} </p>
+
+
+            <div class="players-container">
+
+                <player-card
+                    v-for="player in players"
+                    v-bind:key="player.num"
+                    v-bind:player="player"
+                />
+            </div>
+
+            <button class="main-button">Generate Teams</button>
+
+        </div>
+    </div>
+    
+</template>
+
+<script>
+
+import PlayerCard from './PlayerCard.vue';
+
+export default {
+    name: 'TheGameSession',
+    components: { PlayerCard },
+    sockets: {
+        playerAdded: function(playerName){
+            this.playerNames.push(playerName);
+        }
+    },
+    data: function () {
+        return {
+            code: '',
+            players: []
+        }
+    },
+    mounted: function () {
+        const vm = this
+        this.$socket.emit('create-session', '', function(code) {
+            vm.code = code
+        });
+
+    }
+}
+</script>
+
+<style lang="scss">
+
+.game-code-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  position: relative;
+  height:100%;
+  width:100%;
+  margin:0;
+  padding:0;
+}
+
+.game-code-container h1 {
+    font-size:72px;
+    margin: 80px auto 0px auto;
+}
+
+.game-code-container button {
+    margin-top:16px;
+}
+
+h2 {
+    margin: 0 auto;
+    font-family: "Poppins", sans-serif;
+    font-weight:600;
+    font-size:24px;
+}
+
+.game-code {
+    text-transform: uppercase;
+    color:#522875;
+    padding:24px 80px 13px 80px;
+    font-family: "Luckiest Guy", sans-serif;
+    background-color:white;
+    font-size:40px;
+    margin:8px 0px 0px 0px;
+    border-radius: 20px;
+}
+
+.players-container {
+    width:836px;
+    height:271px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items:flex-start;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin:16px 0px;
+}
+
+
+</style>
